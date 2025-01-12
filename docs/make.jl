5efe69ba3c2021475c369579880bc89884a05105
Abhro R <5664668+abhro@users.noreply.github.com>
2025-01-12 13:30:02 -0500

    Add cross-ref links plugin for unitful.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,27 @@
 using UnitfulGaussian
 using Documenter
+using DocumenterInterLinks
 
 DocMeta.setdocmeta!(UnitfulGaussian, :DocTestSetup, :(using UnitfulGaussian); recursive=true)
 
+links = InterLinks(
+    "Unitful" => "https://painterqubits.github.io/Unitful.jl/stable/",
+)
+
 makedocs(;
-    modules=[UnitfulGaussian],
-    authors="Abhro R. and contributors",
-    sitename="UnitfulGaussian.jl",
-    format=Documenter.HTML(;
+    modules = [UnitfulGaussian],
+    authors = "Abhro R. and contributors",
+    sitename = "UnitfulGaussian.jl",
+    format = Documenter.HTML(;
         edit_link="main",
         assets=String[],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
         "Reference table" => "reference-table.md",
         "Closures" => "closures.md",
     ],
+    plugins = [links],
 )
 
 deploydocs(;
