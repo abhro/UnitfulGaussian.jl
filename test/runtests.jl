b5db0b359a8c103a19f36f63d161600da6886ce8
abhro <5664668+abhro@users.noreply.github.com>
2025-03-25 11:49:22 -0400

    Add aqua tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using UnitfulGaussian
 using Test
+using Aqua
 
 @testset "UnitfulGaussian.jl" begin
+    @testset "Code quality (Aqua)" begin
+        Aqua.test_all(UnitfulGaussian)
+    end
+
     # Write your tests here.
 end
